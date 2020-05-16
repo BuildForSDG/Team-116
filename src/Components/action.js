874fdcf46b057c3f0dc3c17f5bbf7d5fb4729cd8
Quadrizz <mackquadrizz@gmail.com>
2020-05-16 12:14:38 +0100

    Image slider update and other changes to the landing page

--- src/Components/action.js
+++ src/Components/action.js
@@ -3,56 +3,63 @@ import Mentor from '../Assets/Images/mentor.jpg';
 import Analysis from '../Assets/Images/analysis.jpg';
 import Funds  from '../Assets/Images/funds.jpg';
 import Investors from '../Assets/Images/investors.jpg';
+import Main from '../Assets/Images/Capture.JPG'
 import '../Assets/Style/style.css';
 
-let mentor = "Our Mentorship program will help you take your business to the next level";
+let main = "Connect, Grow<br> and Scale up<br>your Business Today!";
+let mentor = "Our Mentorship program,<br>will help you take your<br> business to the next level";
 let analysis = "Realtime Analysis of your business";
-let funds = "Access to loans and investments"
-let investors = "Connecting SME owners to investors"
+let funds = "Access to loans and investments";
+let investors = "Connecting SME owners to investors";
 
-const notes = [mentor , analysis , funds , investors];
+let mainP = "Join a scalable management information System<br>Which will Connect SMEs within Africa<br>with credit Facility providers.....";
+let mentorP = "You're just a few clicks away from learning the best business practices";
+let analysisP = "Spot on analysis about your business<br>making things easier";
+let fundsP = "Quick access to funds to help set up your business";
+let investorsP = "Bridging the gap between investors and SME owners";
 
-const slides = [Mentor, Analysis, Funds, Investors];
-var currentslide = 0;
-var number0fslides = slides.length - 1;
 
+const notes = [main , mentor , analysis , funds , investors];
+const notesP = [mainP , mentorP , analysisP , fundsP, investorsP];
+const slides = [Main, Mentor, Analysis, Funds, Investors];
 
-function loader() {
-    changeimage();
-}
+function loader(n){
+    document.getElementById('h').innerHTML = `${notes[n]}`;
+    document.getElementById('p').innerHTML = `${notesP[n]}`;
+    document.getElementById('image').src = `${slides[n]}`
+} 
 
-function changeimage() {
-    console.log("changeimage function");
-    if (currentslide > number0fslides) {
-        currentslide = 0
-    }
-    document.getElementById("image").src = `${slides[currentslide]}`;
-    document.getElementById("ip").innerHTML = `${notes[currentslide]}`
-    currentslide++;
-    setTimeout(changeimage, 3000);
-
+function test(){
+    console.log("test")
 }
 
 class Action extends React.Component{
     componentDidMount(){
-        loader()
+        loader(0);
     }
+
+    
     render(){
         return(
             <div id = "action" className=''>
                 <div id = "sub1">
                     <div className='mt5'>
-                        <h1 className = 'ml3 mt5 fw9 white'>Connect, and Grow Scale up your  Business Today!</h1>
-                        <p className = ' ml3 f3 white'>Join a scalable management information System<br></br>
-                        Which will Connect SMEs within Africa<br></br>
-                        with credit Facility providers.....
-                        </p>
-                        <button className = 'ml3  bg-white  grow pointer br2'>Get Started</button>
+                        <h1 className = 'ml3 mt5 fw2 gray f1' id = 'h'></h1>
+                        <p className = ' ml3 f3 fw1 dark-blue' id = 'p'></p>
+                        <div id = "butt">
+                            <button className = 'ml3  bg-blue white grow pointer br2'>Get Started</button>
+                        </div>
                     </div>
                     <div id = "illustrat" className = 'br4'>
-                        <img height="500px" width = "auto" id="image" className = "br4"></img>
-                        <p id = "ip" className = "tc "></p>
+                        <img height="500px"  id="image" className = "br4"></img>
                     </div>
+                </div>
+                <div id ="sub2">
+                    <div onClick = {()=>loader(0)}>.</div>
+                    <div onClick = {()=>loader(1)}>.</div>
+                    <div onClick = {()=>loader(2)}>.</div>
+                    <div onClick = {()=>loader(3)}>.</div>
+                    <div onClick = {()=>loader(4)}>.</div>
                 </div>
             </div>
         )
--- src/Components/header.js
+++ src/Components/header.js
@@ -4,11 +4,19 @@ import '../Assets/Style/style.css';
 const Header = ()=>{
     return(
         <div id ='header'>
-            <h1 className = 'ml3 fw-bold '>SMEs Fund Cycle</h1>
-            
-                <p className = 'h2 mt3 dim   mr3 white pointer underline'>Log In</p>
-                <p className = 'h2 mt3  dim  mr3  white pointer underline'>Sign Up</p>
-            
+            <h1 className = 'ml3 fw-bold dark-blue'>SMEs Fund Cycle</h1>
+            <div id='d1'>
+                <p className = 'gray underline pointer grow'>Partners</p>
+                <p className = 'gray underline pointer grow'>Blogs</p>
+                <p className = 'gray underline pointer grow'>Events</p>
+            </div>
+            <div id = 'd2'>
+                <input type = 'search' placeholder = 'Search for Business'></input>
+            </div>
+            <div>
+                <button className = 'h2 mt3 dim   mr3 bg-blue pointer white bw0 w4 br4'>Log In</button>
+                <button className = 'h2 mt3  dim  mr3  bg-blue pointer white bw0 w4 br4'>Sign Up</button>   
+            </div>
         </div>
     )
 }
