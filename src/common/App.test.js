ac8adc6621a813881b62445f15898a0daf24a9b5
antony_kariu <kariuantony@gmail.com>
2020-05-31 00:48:02 +0300

    Removing files,
    removes files from the develop2 repo

--- src/common/App.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * React Starter Kit for Firebase
- * https://github.com/kriasoft/react-firebase-starter
- * Copyright (c) 2015-present Kriasoft | MIT License
- */
-
-it('renders without crashing', () => {
-  // TODO: Implement a unit test
-});
--- src/server/mutations/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * React Starter Kit for Firebase
- * https://github.com/kriasoft/react-firebase-starter
- * Copyright (c) 2015-present Kriasoft | MIT License
- */
-
-export * from './user';
-export * from './story';
